c6a2138b24d5177eb2c8ddc64ea24d24f18982d6
leonardtang <leonardgentwintang@gmail.com>
2020-08-04 00:03:27 -0400

    updating for TF 2.1

--- test.py
+++ test.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 from pycocotools.coco import COCO
 from keras.models import Model, load_model
@@ -7,7 +6,6 @@ from keras.callbacks import ModelCheckpoint
 from keras.optimizers import Adam
 import scipy.ndimage as ndimage
 import scipy.misc as misc
-
 
 coco = COCO('scripts/models/peripheralnet_v1/scripts/annotations/instances_val2014.json')
 
@@ -21,10 +19,10 @@ model = load_model('/n/coxfs01/kevin_wu/pnet_v2/model_weights/add_none_025_from1
 print(model.summary())
 
 model.compile(
-	loss = 'categorical_crossentropy', \
-	optimizer = Adam(1e-4, beta_1=.9, beta_2=.99, decay = .3),\
-	metrics = ['accuracy']
-	)
+    loss='categorical_crossentropy',
+    optimizer=Adam(1e-4, beta_1=.9, beta_2=.99, decay=.3),
+    metrics=['accuracy']
+)
 
 img_data = np.load('scripts/models/peripheralnet_v1/results/boxes.npy')
 
@@ -44,79 +42,74 @@ all_testing = []
 
 for i in range(np.shape(img_data)[0]):
 
-	print(i)
+    print(i)
 
-	img_line = img_data[i]
+    img_line = img_data[i]
 
-	image_id = int(img_line[0])
-	ann_ids = int(img_line[6])
-	annotations = coco.loadAnns(ann_ids)[0]
-	gt_vector = coco.getCatIds().index(int(img_line[5]))
-	object_mask = coco.annToMask(annotations)
+    image_id = int(img_line[0])
+    ann_ids = int(img_line[6])
+    annotations = coco.loadAnns(ann_ids)[0]
+    gt_vector = coco.getCatIds().index(int(img_line[5]))
+    object_mask = coco.annToMask(annotations)
 
-	try:
-		obj_im = fit_in_square(apply_mask(coco, raw_image = load_image(image_id), mask = object_mask, keep = 'object', crop = 0.25), object_dim)
-	except:
-		raise ValueError('wrong')
-		continue
+    try:
+        obj_im = fit_in_square(
+            apply_mask(coco, raw_image=load_image(image_id), mask=object_mask, keep='object', crop=0.25), object_dim)
+    except:
+        raise ValueError('wrong')
+        continue
 
-	object_input = prepare_input(obj_im)
-	# all_testing.append(prepare_input(apply_mask(coco, raw_image = load_image(image_id), mask = object_mask, keep = 'object', crop = 0)))
+    object_input = prepare_input(obj_im)
+    # all_testing.append(prepare_input(apply_mask(coco, raw_image = load_image(image_id), mask = object_mask, keep = 'object', crop = 0)))
 
-	if add == None:
-		model_pred = model.predict({'object_input': object_input})
+    if add == None:
+        model_pred = model.predict({'object_input': object_input})
 
-	if add == 'gist':
-		context_input = prepare_input(fit_in_square(apply_mask(coco, raw_image = load_image(image_id), mask = object_mask, keep = 'context', crop = 0), 448))
+    if add == 'gist':
+        context_input = prepare_input(
+            fit_in_square(apply_mask(coco, raw_image=load_image(image_id), mask=object_mask, keep='context', crop=0),
+                          448))
 
-		model_pred = model.predict({'object_input': object_input, 'scene_input': context_input})
+        model_pred = model.predict({'object_input': object_input, 'scene_input': context_input})
 
-	if add == 'spotlight_dt':
+    if add == 'spotlight_dt':
+        context_image = fit_in_square(load_image(image_id), object_dim)
+        # misc.imsave('scripts/models/pnet_v2/sample_images/context'+str(image_id)+'.png', np.rollaxis(context_image, 0, 3))
+        context_input = prepare_input(context_image)
+        object_mask = fit_in_square(prepare_mask(object_mask), object_dim)
+        # object_mask_show = object_mask
+        # object_mask_show[0] = 255.
+        # misc.imsave('scripts/models/pnet_v2/sample_images/mask'+str(image_id)+'.png', np.rollaxis(object_mask_show, 0, 3))
+        object_mask = object_mask[0]
+        # print(np.shape(np.where(object_mask)))
+        # misc.imsave('scripts/models/pnet_v2/sample_images/mask'+str(image_id)+'.png', np.rollaxis(ndimage.distance_transform_cdt(object_mask), 0, 3))
+        distance_mask = np.expand_dims(ndimage.distance_transform_cdt(object_mask), axis=0)
+        object_mask = np.expand_dims(object_mask, axis=0)
+        combined_mask = np.expand_dims(np.concatenate((object_mask, distance_mask), axis=0), axis=0)
 
-		context_image = fit_in_square(load_image(image_id), object_dim)
-		# misc.imsave('scripts/models/pnet_v2/sample_images/context'+str(image_id)+'.png', np.rollaxis(context_image, 0, 3))
-		context_input = prepare_input(context_image)
-		object_mask = fit_in_square(prepare_mask(object_mask), object_dim)
-		# object_mask_show = object_mask
-		# object_mask_show[0] = 255.
-		# misc.imsave('scripts/models/pnet_v2/sample_images/mask'+str(image_id)+'.png', np.rollaxis(object_mask_show, 0, 3))
-		object_mask = object_mask[0]
-		# print(np.shape(np.where(object_mask)))
-		# misc.imsave('scripts/models/pnet_v2/sample_images/mask'+str(image_id)+'.png', np.rollaxis(ndimage.distance_transform_cdt(object_mask), 0, 3))
-		distance_mask = np.expand_dims(ndimage.distance_transform_cdt(object_mask), axis = 0)
-		object_mask = np.expand_dims(object_mask, axis = 0)
-		combined_mask = np.expand_dims(np.concatenate((object_mask, distance_mask), axis=0), axis = 0)
-		
-		model_pred = model.predict({'object_input': object_input, 'spotlight_input': combined_mask, 'context_input': context_input})
+        model_pred = model.predict(
+            {'object_input': object_input, 'spotlight_input': combined_mask, 'context_input': context_input})
 
-	# if add == 'spotlight':
+    # if add == 'spotlight':
 
-	# 	context_input = prepare_input(fit_in_square(load_image(image_id), object_dim))
-	# 	object_mask = fit_in_square(prepare_mask(object_mask), object_dim)[0]
-	# 	object_mask = np.expand_dims(np.expand_dims(object_mask, axis = 0), axis = 0)
-		
-	# 	model_pred = model.predict({'object_input': object_input, 'spotlight_input': object_mask, 'context_input': context_input})
+    # 	context_input = prepare_input(fit_in_square(load_image(image_id), object_dim))
+    # 	object_mask = fit_in_square(prepare_mask(object_mask), object_dim)[0]
+    # 	object_mask = np.expand_dims(np.expand_dims(object_mask, axis = 0), axis = 0)
 
-	top5 = model_pred[0].argsort()[-5:]
+    # 	model_pred = model.predict({'object_input': object_input, 'spotlight_input': object_mask, 'context_input': context_input})
 
-	top1_sum += int(gt_vector in top5[-1:])
-	top3_sum += int(gt_vector in top5[-3:])
-	top5_sum += int(gt_vector in top5)
-	total_count += 1
+    top5 = model_pred[0].argsort()[-5:]
 
-	print(total_count)
+    top1_sum += int(gt_vector in top5[-1:])
+    top3_sum += int(gt_vector in top5[-3:])
+    top5_sum += int(gt_vector in top5)
+    total_count += 1
+
+    print(total_count)
 
 # np.save('/n/coxfs01/kevin_wu/pnet_v2/model_weights/1000testing.npy', all_testing)
 
 print('25%')
-print('Top 1 Average: ' + str(top1_sum*1./total_count))
-print('Top 3 Average: ' + str(top3_sum*1./total_count))
-print('Top 5 Average: ' + str(top5_sum*1./total_count))
-
-
-
-
-
-
-
-
+print('Top 1 Average: ' + str(top1_sum * 1. / total_count))
+print('Top 3 Average: ' + str(top3_sum * 1. / total_count))
+print('Top 5 Average: ' + str(top5_sum * 1. / total_count))
--- train.py
+++ train.py
@@ -8,49 +8,41 @@ from keras.callbacks import ModelCheckpoint
 from keras_diagram import ascii
 from keras.optimizers import Adam
 
+
 def train(add, num_testing, object_dim, job_name, **kwargs):
+    coco_train = COCO('/home/leonard/../../media/data/Datasets/MSCOCO2014/annotations/instances_train2014.json')
+    coco_test = COCO('/home/leonard/../../media/data/Datasets/MSCOCO2014/annotations/instances_val2014.json')
 
-	coco_train = COCO('/home/leonard/../../media/data/Datasets/MSCOCO2014/annotations/instances_train2014.json')
-	coco_test = COCO('/home/leonard/../../media/data/Datasets/MSCOCO2014/annotations/instances_val2014.json')
+    training_generator = create_generator(coco=coco_train, mode='training', add=add, object_dim=object_dim, **kwargs)
+    testing_generator = create_generator(coco=coco_test, mode='testing', add=add, object_dim=object_dim, **kwargs)
 
-	training_generator = create_generator(coco = coco_train, mode = 'training', add = add, object_dim = object_dim, **kwargs)
-	testing_generator = create_generator(coco = coco_test, mode = 'testing', add = add, object_dim = object_dim, **kwargs)
+    model = build_model(add=add, object_dim=object_dim, **kwargs)
 
-	model = build_model(add = add, object_dim = object_dim, **kwargs)
+    model.compile(loss='categorical_crossentropy', optimizer=Adam(1e-6, beta_1=.9, beta_2=.99), metrics=['accuracy'])
 
-	model.compile(loss = 'categorical_crossentropy', optimizer = Adam(1e-6, beta_1=.9, beta_2=.99), metrics = ['accuracy'])
+    callbacks_list = [
+        ModelCheckpoint('../model_weights/' + job_name + '.h5', monitor='val_loss', verbose=1, save_best_only=True,
+                        mode='min')]
 
-	callbacks_list = [ModelCheckpoint('../model_weights/'+job_name+'.h5', monitor='val_loss', verbose=1, save_best_only=True, mode='min')]
+    print(model.summary())
 
-	print(model.summary())
+    history = model.fit(
+        training_generator,
+        validation_steps=num_testing,
+        validation_data=testing_generator,
+        steps_per_epoch=5000,
+        epochs=500,
+        callbacks=callbacks_list,
+        verbose=2,
+        max_queue_size=10,
+        workers=1,
+    )
 
-	history = model.fit_generator(
-		training_generator, \
-		validation_steps = num_testing, \
-		validation_data = testing_generator, \
-		steps_per_epoch = 5000, \
-		epochs = 500, \
-		callbacks = callbacks_list,\
-		verbose=2, \
-		max_queue_size = 10, \
-		workers = 1, \
-		)
 
 if __name__ == '__main__':
-
-	add = "gist"
-
-	num_testing = 1000
-
-	object_dim = 224
-
-	context_dim = 448
-
-	job_name = 'JOB_NAME'
-
-	train(add, num_testing, object_dim, job_name, context_dim = context_dim, testing_size = num_testing)
-
-
-
-
-
+    add = "gist"
+    num_testing = 1000
+    object_dim = 224
+    context_dim = 448
+    job_name = 'JOB_NAME'
+    train(add, num_testing, object_dim, job_name, context_dim=context_dim, testing_size=num_testing)
